9621741253935179dd60b1186b438a9b2b1eee33
davidvishr <davidvishwakarma6991@gmail.com>
2024-01-14 21:48:26 +0530

    node

--- index.js
+++ index.js
@@ -22,32 +22,9 @@ app.get("/", (req, res) => {
   res.send({ title: "Social App Node js Test" });
 });
 
-app.get("/add-node", async (req, res) => {
-  try {
-    await Book.insertMany[
-      ({
-        title: "Sone of Book",
-        body: "Body Text Check",
-      },
-      {
-        title: "Sone of Book",
-        body: "Body Text Check",
-      })
-    ];
-    res.send('Added Data')
-  } catch (error) {
-    console.log(error);
-  }
-});
 
-app.get("/books", async (req, res) => {
-  const book = await Book.find();
-  if (book) {
-    res.json(book);
-  } else {
-    res.send("Something went wrong.");
-  }
-});
+
+
 
 connectDB()
   .then(() => {
